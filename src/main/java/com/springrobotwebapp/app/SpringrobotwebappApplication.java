ddb34f52d04a0dec9231b03b4e1fed93a68a38fd
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2019-10-28 19:47:22 +0100

    Main spring class and test

---
+++ src/main/java/com/springrobotwebapp/app/SpringrobotwebappApplication.java
@@ -0,0 +1,13 @@
+package com.springrobotwebapp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrobotwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrobotwebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springrobotwebapp/app/SpringrobotwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrobotwebapp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrobotwebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
