a1cfaf3df356d636942b8c379f4b30b771de861a
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2019-10-28 19:36:11 +0100

    Started by Spring Initializr

---
+++ src/main/java/com/springrobotwebapp/springrobotwebapp/SpringrobotwebappApplication.java
@@ -0,0 +1,13 @@
+package com.springrobotwebapp.springrobotwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrobotwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrobotwebappApplication.class, args);
+    }
+
+}
