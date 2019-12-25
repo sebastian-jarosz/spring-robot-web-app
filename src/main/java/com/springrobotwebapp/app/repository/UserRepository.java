4d499925f9cac8e61a0a00a7856df5c753de7220
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2019-12-25 17:57:46 +0100

    clear

--- src/main/java/com/springrobotwebapp/app/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springrobotwebapp.app.repository;
-
-import com.springrobotwebapp.app.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
